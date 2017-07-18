77916e35f61f33f00994658d78ff804bcd910ddf
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-07-17 19:31:57 -0700

    added smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,36 @@
-// subscription alert
-$('.btn[type=submit]').on('click', function() {
-  // check for validation
-  if (!$('input[type=email]').val()) {
-    alert('Please provide your email address');
-  } else {
-    // thank user for subbing
-    alert('Thanks for subscribing!');
-    // reset value
-    $('input[type=email]').val('');
-  }
-});
+$(document).ready(function(){
+  // subscription alert
+  $('.btn[type=submit]').on('click', function() {
+    // check for validation
+    if (!$('input[type=email]').val()) {
+      alert('Please provide your email address');
+    } else {
+      // thank user for subbing
+      alert('Thanks for subscribing!');
+      // reset value
+      $('input[type=email]').val('');
+    }
+  });
 
-// cart scripts
+  // cart scripts
 
-var itemCounter = 0;
+  var itemCounter = 0;
 
-$('.add-to-cart').on('click', function() {
-  itemCounter++;
-  $('.item-counter').html('<p>' + itemCounter + '</p>');
-  $('.item-counter').css('display', 'block');
+  $('.add-to-cart').on('click', function() {
+    itemCounter++;
+    $('.item-counter').html('<p>' + itemCounter + '</p>');
+    $('.item-counter').css('display', 'block');
+  });
+
+  // smooth scroll
+  $(document).on('click', 'a', function(event){
+    // Get the height of the header
+    var headerHeight = $('header').height();
+
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top - $('header').height()
+    }, 500);
+  });
 });
