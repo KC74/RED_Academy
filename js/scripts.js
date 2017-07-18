4e3900a1522cdccdf8b6b8d34692363725fa0502
Kenneth Chow <Ken@Kenneths-MacBook-Pro.local>
2017-07-17 17:55:30 -0700

    Added add to cart buttons and functionality. Added cart item counter.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
-$('.main-carousel').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true
+// subscription alert
+$('.btn[type=submit]').on('click', function() {
+  // check for validation
+  if (!$('input[type=email]').val()) {
+    alert('Please provide your email address');
+  } else {
+    // thank user for subbing
+    alert('Thanks for subscribing!');
+    // reset value
+    $('input[type=email]').val('');
+  }
+});
+
+// cart scripts
+
+var itemCounter = 0;
+
+$('.add-to-cart').on('click', function() {
+  itemCounter++;
+  $('.item-counter').html('<p>' + itemCounter + '</p>');
+  $('.item-counter').css('display', 'block');
 });
